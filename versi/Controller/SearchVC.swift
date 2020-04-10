d41a2304fff43e428352b98d80ac53f47c6c2a21
Kerim Deveci <beyzaybikerim@gmail.com>
2020-04-10 18:09:54 +0300

    tabbar icon added

---
+++ versi/Controller/SearchVC.swift
@@ -0,0 +1,30 @@
+//
+//  SearchVC.swift
+//  versi
+//
+//  Created by Kerim Deveci on 10.04.2020.
+//  Copyright © 2020 Kerim Deveci. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

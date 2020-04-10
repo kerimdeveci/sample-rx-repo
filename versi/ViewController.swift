b023fa8cb3de0a5cc64b67cf916f92d49d31b9ec
Kerim Deveci <beyzaybikerim@gmail.com>
2020-04-10 17:23:24 +0300

    Initial Commit

---
+++ versi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  versi
+//
+//  Created by Kerim Deveci on 10.04.2020.
+//  Copyright © 2020 Kerim Deveci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
